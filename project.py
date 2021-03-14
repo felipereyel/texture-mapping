926130dba33c5c8870473900956b67b45151ab94
felipe reyel <felipe.reyel@gmail.com>
2021-03-14 20:30:13 -0300

    refact

--- project.py
+++ project.py
@@ -1,27 +1,29 @@
 import sys
-import numpy as np
 from PIL import Image
+import numpy as np
 
 def solve_transformation(p, q):
 	a = np.array([
-		[ p[0][0], p[0][1], p[0][2], 0, 	  0, 	   0, 		0, 		0, 		0, 		   0, 		 0, 	   0        ],
-		[ 0, 	   0, 		0, 		 p[0][0], p[0][1], p[0][2], 0, 		0, 		0, 		   0, 		 0, 	   0        ],
-		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		p[0][0], p[0][1], p[0][2], 0, 		 0,    	   0        ],
+		[ p[0][0], p[0][1], p[0][2], 0, 	  0, 	   0, 		0, 		 0, 	  0, 	   -q[0][0], 0, 	   0,        0        ],
+		[ 0, 	   0, 		0, 		 p[0][0], p[0][1], p[0][2], 0, 		 0, 	  0, 	   -q[0][1], 0, 	   0, 	     0        ],
+		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		p[0][0], p[0][1], p[0][2], -q[0][2], 0, 	   0,        0        ],
 
-		[ p[1][0], p[1][1], p[1][2], 0, 	  0, 	   0, 	 	0, 		 0, 	  0, 	   -q[1][0], 0, 	   0        ],
-		[ 0, 	   0, 		0, 		 p[1][0], p[1][1], p[1][2], 0, 		 0, 	  0, 	   -q[1][1], 0, 	   0        ],
-		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		p[1][0], p[1][1], p[1][2], -q[1][2], 0, 	   0        ],
+		[ p[1][0], p[1][1], p[1][2], 0, 	  0, 	   0, 	 	0, 		 0, 	  0, 	   0, 		 -q[1][0], 0, 	     0        ],
+		[ 0, 	   0, 		0, 		 p[1][0], p[1][1], p[1][2], 0, 		 0, 	  0, 	   0, 		 -q[1][1], 0, 	     0        ],
+		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		p[1][0], p[1][1], p[1][2], 0, 		 -q[1][2], 0, 	     0        ],
 
-		[ p[2][0], p[2][1], p[2][2], 0, 	  0, 	   0, 		0, 		 0, 	  0, 	   0, 		 -q[2][0], 0        ],
-		[ 0, 	   0, 		0, 		 p[2][0], p[2][1], p[2][2], 0, 		 0, 	  0, 	   0, 	     -q[2][1], 0        ],
-		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		p[2][0], p[2][1], p[2][2], 0, 		 -q[2][2], 0        ],
+		[ p[2][0], p[2][1], p[2][2], 0, 	  0, 	   0, 		0, 		 0, 	  0, 	   0, 		 0, 	   -q[2][0], 0        ],
+		[ 0, 	   0, 		0, 		 p[2][0], p[2][1], p[2][2], 0, 		 0, 	  0, 	   0, 		 0, 	   -q[2][1], 0        ],
+		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		p[2][0], p[2][1], p[2][2], 0, 		 0, 	   -q[2][2], 0        ],
 
-		[ p[3][0], p[3][1], p[3][2], 0, 	  0, 	   0, 		0, 		 0, 	  0, 	   0, 		 0, 	   -q[3][0] ],
-		[ 0, 	   0,		0,	 	 p[3][0], p[3][1], p[3][2], 0, 		 0, 	  0, 	   0, 		 0, 	   -q[3][1] ],
-		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		p[3][0], p[3][1], p[3][2], 0, 		 0, 	   -q[3][2] ]
+		[ p[3][0], p[3][1], p[3][2], 0, 	  0, 	   0, 		0, 		 0, 	  0, 	   0, 		 0, 	   0, 	     -q[3][0] ],
+		[ 0, 	   0,		0,	 	 p[3][0], p[3][1], p[3][2], 0, 		 0, 	  0, 	   0, 		 0, 	   0, 	     -q[3][1] ],
+		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		p[3][0], p[3][1], p[3][2], 0, 		 0, 	   0, 	     -q[3][2] ],
+
+		[ 0, 	   0, 		0, 		 0, 	  0, 	   0, 		0, 		 0, 	  0, 	   1, 		 0, 	   0, 	    0 ]
 	])
 
-	b = np.array([q[0][0], q[0][1], q[0][2], 0, 0, 0, 0, 0, 0, 0, 0, 0])
+	b = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
 	x = np.linalg.solve(a, b)
 	return np.linalg.inv(np.array([
@@ -29,9 +31,6 @@ def solve_transformation(p, q):
 		[x[3], x[4], x[5]],
 		[x[6], x[7], x[8]]
 	]))
-
-def is_inside(x, y, w, h):
-	return 
 
 def texture_color(x, y, w, h, c):
 	return c[round(x) % w, -round(y) % h]
@@ -44,41 +43,41 @@ if __name__ == "__main__":
 	texture = Image.open(sys.argv[1])
 	original = Image.open(sys.argv[2])
 
-	wT, hT = texture.size
-	cT = texture.load()
+	texture_width, texture_height = texture.size
+	texture_colors = texture.load()
 
-	wO, hO = original.size
-	cO = original.load()
+	orig_width, orig_height = original.size
 
-	p = [
-		[0,hT-1,1],
-		[wT-1,hT-1,1],
-		[wT-1,0,1],
+	texture_points = [
+		[0,texture_height-1,1],
+		[texture_width-1,texture_height-1,1],
+		[texture_width-1,0,1],
 		[0,0,1],
 	]
 
-	q = []
+	original_points = []
 	for i in range(4):
-		q.append([int(sys.argv[3+2*i]), int(sys.argv[4+2*i]), 1])
+		original_points.append([int(sys.argv[3+2*i]), int(sys.argv[4+2*i]), 1])
+		print(original_points[i], "->", texture_points[i])
 
 	# transformation matrix
-	tranformation = solve_transformation(p, q)
+	tranformation = solve_transformation(texture_points, original_points)
 
 	# iterate over all pixels of original image
-	for xO in range(wO):
-		for yO in range(hO):
-			original_point = (xO, yO, 1) # RP2
+	for x_orig in range(orig_width):
+		for y_orig in range(orig_height):
+			original_point = (x_orig, y_orig, 1) # RP2
 
 			# corresponding point in texture
 			texture_point = np.dot(tranformation, original_point) # RP2
-			xT = texture_point[0]/texture_point[2]
-			yT = texture_point[1]/texture_point[2] 
+			x_texture = texture_point[0]/texture_point[2]
+			y_texture = texture_point[1]/texture_point[2] 
 
 			# if inside texture rectangle apply color
-			if 0 < xT and xT < wT and 0 < yT and yT < hT:
+			if 0 < x_texture and x_texture < texture_width and 0 < y_texture and y_texture < texture_height:
 				original.putpixel(
-					(xO,yO), 
-					texture_color(xT, yT, wT, hT, cT)
+					(x_orig,y_orig), 
+					texture_color(x_texture, y_texture, texture_width, texture_height, texture_colors)
 				)
 
 	if len(sys.argv) == 12:
